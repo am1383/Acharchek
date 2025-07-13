1e50ad3c8a388da538698ef6b8ff1dacda3100fb
Amir Mohammad <am205379@gmail.com>
2025-07-13 16:51:45 -0500

    Feat(Resources): Implement Auth resources

--- app/Modules/Auth/Resources/LoginResource.php
+++ app/Modules/Auth/Resources/LoginResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\Auth\Resources;
 
+use App\Helpers\DateFormatHelper;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,22 @@ class LoginResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'phone' => $this->phone,
+            'full_name' => $this->full_name,
+            'sms_credit' => $this->sms_credit,
+            'email' => $this->email,
+
+            'email_status' => [
+                'verified' => $this->email_verified_at ? true : false,
+                'verify_date' => $this->email_verified_at ? DateFormatHelper::dateTime($this->email_verified_at) : null,
+            ],
+
+            'national_code' => $this->national_code,
+            'birth_date' => $this->birth_date ? DateFormatHelper::dateTime($this->birth_date) : null,
+            'ban' => $this->ban ? true : false,
+            'registered_at' => DateFormatHelper::dateTime($this->registered_at),
+        ];
     }
 }

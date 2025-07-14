d2e51b3fc0b837552c9a9380e7d709f5046ed37a
Amir Mohammad <am205379@gmail.com>
2025-07-14 14:57:18 -0500

    Feat(Docs): Append docs to business and sms config

--- config/sms.php
+++ config/sms.php
@@ -1,20 +1,31 @@
 <?php
 
 return [
-    'max_user_own_businesses' => 5,
-    'max_user_join_businesses' => 15,
 
-    'phone_verification_code_length' => 5,  // min:3  max:6  به هیچ عنوان نباید طول کد از 3 کمتر و از 3 بیشتر باشد
+   /*
+    |--------------------------------------------------------------------------
+    | Phone verification code length
+    |--------------------------------------------------------------------------
+    |
+    | Show cost for every group of users in toman (In every 65 characters)
+    |
+    */
 
-    'max_business_customer_groups' => 15, // بیشتر از ۱۵ گروه نمیتوانید ثبت کنید!
+    'phone_verification_code_length' => 5,
 
-    'business_new_customers_per_hour' => 30, // هر کسب و کار 30 تا مشتری میتونه در ساعت ثبت کنه!
-
+    /*
+    |--------------------------------------------------------------------------
+    | SMS Cost
+    |--------------------------------------------------------------------------
+    |
+    | Show cost for every group of users in toman (In every 65 characters)
+    |
+    */
     'sms_cost' => [
-        'service' => 300, // toman
-        'remember' => 300, // toman
-        'single' => 100, // toman به ازای هر 65 کاراکتر
-        'group' => 100, // toman به ازای هر 65 کاراکتر
-        'extra' => 100, // مبلغ اضافی برای افزودن آدرس و ایدی اینستاگرام به پیامک های سرویس و یادآوری
+        'service' => 300,
+        'remember' => 300,
+        'single' => 100,
+        'group' => 100,
+        'extra' => 100,
     ],
 ];

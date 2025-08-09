ac929f4794c8cfe3178be5d9ff48fa863e102676
Amir Mohammad <am205379@gmail.com>
2025-08-09 16:09:49 -0500

    Remove: Remove unsued repositories and models

--- app/Modules/Auth/Repositories/Contracts/BusinessTypeRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Modules\Auth\Repositories\Contracts;
-
-use App\Core\Repositories\Contracts\BaseRepositoryInterface;
-
-interface BusinessTypeRepositoryInterface extends BaseRepositoryInterface {}

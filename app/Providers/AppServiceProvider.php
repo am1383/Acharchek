f6ba16dc608197a9673515589d6bcb8bd6fa0b11
Amir Mohammad <am205379@gmail.com>
2025-08-09 17:13:50 -0500

    Feat(AppServiceProvider): Refactor service registration and remove repository bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Modules\Admin\Providers\AdminServiceProvider;
 use App\Modules\Auth\Models\City;
 use App\Modules\Auth\Models\Information;
 use App\Modules\Auth\Models\MobileDevice;
 use App\Modules\Auth\Models\Province;
+use App\Modules\Auth\Models\User;
+use App\Modules\Auth\Providers\AuthServiceProvider;
 use App\Modules\Auth\Repositories\Contracts\CityRepositoryInterface;
 use App\Modules\Auth\Repositories\Contracts\InformationRepositoryInterface;
 use App\Modules\Auth\Repositories\Contracts\MobileDeviceRepositoryInterface;
@@ -14,6 +17,10 @@ use App\Modules\Auth\Repositories\Eloquent\CityRepository;
 use App\Modules\Auth\Repositories\Eloquent\InformationRepository;
 use App\Modules\Auth\Repositories\Eloquent\MobileDeviceRepository;
 use App\Modules\Auth\Repositories\Eloquent\ProvinceRepository;
+use App\Modules\Panel\Providers\PanelServiceProvider;
+use App\Modules\Panel\Services\PanelService;
+use App\Modules\SMS\Providers\SMSServiceProvider;
+use App\Modules\User\Providers\UserServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,21 +30,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(CityRepositoryInterface::class, function ($app): CityRepositoryInterface {
-            return new CityRepository($app->make(City::class));
-        });
-
-        $this->app->bind(ProvinceRepositoryInterface::class, function ($app): ProvinceRepositoryInterface {
-            return new ProvinceRepository($app->make(Province::class));
-        });
-
-        $this->app->bind(InformationRepositoryInterface::class, function ($app): InformationRepositoryInterface {
-            return new InformationRepository($app->make(Information::class));
-        });
-
-        $this->app->bind(MobileDeviceRepositoryInterface::class, function ($app): MobileDeviceRepositoryInterface {
-            return new MobileDeviceRepository($app->make(MobileDevice::class));
-        });
+        $this->app->register(AdminServiceProvider::class);
+        $this->app->register(AuthServiceProvider::class);
+        $this->app->register(UserServiceProvider::class);
+        $this->app->register(PanelServiceProvider::class);
+        $this->app->register(SMSServiceProvider::class);
     }
 
     /**
